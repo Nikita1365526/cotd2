8d5f3e0adbfd9a36acc90a8773ce367561d4eecb
Nikita1365526 <71058682+Nikita1365526@users.noreply.github.com>
2020-10-06 12:51:45 +0000

    Update pets.php

--- data/pets.php
+++ data/pets.php
@@ -25,7 +25,7 @@ $_SESSION['item'][1]['rank'] = 2;
 $_SESSION['item'][1]['caption'] = 'Canberra Cat';
 $_SESSION['item'][1]['trivia'] = "<p>My name is Burley and my post office box is in Canberra. The Government appointed me into a senior position at the Human Rights Commission. </p> <p style='color:silver;font-size:80%;font-style:italic'>Like me and we can obsess over repealing section 18C together.</p>";
 $_SESSION['item'][1]['theme'] = 'cats';
-$_SESSION['item'][1]['filename'] = 'data/images/pets/billie.jpg';
+$_SESSION['item'][1]['filename'] = 'data/images/pets/bogor.jpg';
 $_SESSION['item'][1]['rating'] = 0;
 
 $_SESSION['item'][2]['name'] = 'melbourne';
